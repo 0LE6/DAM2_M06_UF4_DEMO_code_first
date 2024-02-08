83bca80c4e49ce4e55fd5bfaff34f85b4516b6a1
Oleg <kharenkoleg@gmail.com>
2024-02-08 15:44:12 +0100

    <<

---
+++ DAM2_M06_UF4_DEMO_code_first/Model/SchoolDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF4_DEMO_code_first.Model
+{
+    internal class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext() { }
+        public SchoolDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=localhost;Database=SCHOOL;Uid=root;Pwd=\"\"");
+            }
+        }
+        public virtual DbSet<Alumno> Alumnos { get; set; }
+        public virtual DbSet<Curso> Cursos { get; set; }
+    }
+}
