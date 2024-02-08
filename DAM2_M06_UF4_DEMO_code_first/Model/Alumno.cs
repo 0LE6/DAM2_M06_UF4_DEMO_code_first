c59e3ec38df9cee97ea120d3a249cf86f1010987
Oleg <kharenkoleg@gmail.com>
2024-02-08 15:36:38 +0100

    <<

---
+++ DAM2_M06_UF4_DEMO_code_first/Model/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF4_DEMO_code_first.Model
+{
+    public class Alumno
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DataNaixament { get; set; }
+        //public Curso Curso { get; set; }
+
+    }
+}
---
+++ DAM2_M06_UF4_DEMO_code_first/Model/Profesor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF4_DEMO_code_first.Model
+{
+    public class Profesor
+    {
+
+    }
+}
